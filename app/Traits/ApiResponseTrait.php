4afb88e7fb37628b65f67e499c8edd74698ba823
RafiAwes <rafiaweshan4897@gmail.com>
2026-01-05 15:54:28 +0600

    ApiResponseTrait added

---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+    /**
+     * Success Response
+     *
+     * * @param mixed $data
+     */
+    public function successResponse($data, string $message = 'success', int $statusCode = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+
+    public function errorResponse(string $message = 'error', int $statusCode = 400, $error = null): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'status' => 'error',
+            'message' => $message,
+        ];
+        if (! empty($error)) {
+            $response['data'] = $error;
+        }
+
+        return response()->json($response, $statusCode);
+    }
+}
